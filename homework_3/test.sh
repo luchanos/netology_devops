16820701e09fb4a143e15cd78d4b6543443529fe
luchanos <lukash_91@mail.ru>
2022-11-18 00:02:39 +0300

    one more homework

--- homework_3/test.sh
+++ homework_3/test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-for n in {1..300000}; do
-  printf $n
-# shellcheck disable=SC2086
-#  touch $n.bin
-#  [[ -s /tmp ]]: [[printf "sample text" > "$n.bin"]]
-done
+if [[ -d /tmp ]];
+then
+    echo "$DIR directory exists."
+else
+	echo "$DIR directory does not exist."
+fi
 
 #https://stackoverflow.com/questions/3427872/whats-the-difference-between-and-in-bash
